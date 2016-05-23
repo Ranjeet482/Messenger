4533b931ca85d9d702229e8970df7e49a282a9e8
Ranjeet482 <ranjeet.sah@mindtree.com>
2016-05-23 16:46:34 +0530

    Adding two API's

--- src/main/java/com/mindtree/msesss/MessagesResource.java
+++ src/main/java/com/mindtree/msesss/MessagesResource.java
@@ -2,8 +2,11 @@ package com.mindtree.msesss;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -12,13 +15,27 @@ import com.mindtree.msesss.service.MessageServers;
 
 @Path("/messages")
 public class MessagesResource {
-	
+
 	MessageServers messageService = new MessageServers();
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Message> getMessages() {
+		return messageService.getAllMessages();
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message message) {
+		return messageService.addMessages(message);
+	}
 	
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
-	public List<Message> getMessages(){
-		return messageService.getAllMessages();
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message getMessage(@PathParam("messageId") long messageId) {
+		return messageService.getMessages(messageId);
 	}
 
 }

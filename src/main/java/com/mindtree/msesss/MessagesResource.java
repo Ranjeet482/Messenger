4308939b8ad4c3bcff8847bf1609f32d23d79026
Ranjeet482 <ranjeet.sah@mindtree.com>
2016-05-24 15:35:24 +0530

    Implementing Profile Resource

--- src/main/java/com/mindtree/msesss/MessagesResource.java
+++ src/main/java/com/mindtree/msesss/MessagesResource.java
@@ -3,8 +3,10 @@ package com.mindtree.msesss;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -14,26 +16,37 @@ import com.mindtree.msesss.model.Message;
 import com.mindtree.msesss.service.MessageServers;
 
 @Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class MessagesResource {
 
 	MessageServers messageService = new MessageServers();
 
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessages() {
 		return messageService.getAllMessages();
 	}
 
 	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
 	public Message addMessage(Message message) {
 		return messageService.addMessages(message);
 	}
 	
+	@PUT
+	@Path("/{messageId}")
+	public Message updateMessage(@PathParam("messageId") long messageId, Message message){
+		message.setId(messageId);
+		return messageService.updateMessages(message);
+	}
+	
+	@DELETE
+	@Path("/{messageId}")
+	public void removeMessages(@PathParam("messageId") long messageId){
+		messageService.removeMessages(messageId);
+	}
+	
 	@GET
 	@Path("/{messageId}")
-	@Produces(MediaType.APPLICATION_JSON)
 	public Message getMessage(@PathParam("messageId") long messageId) {
 		return messageService.getMessages(messageId);
 	}
